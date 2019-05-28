235895f1bbb630d6013f987763fe0e915a333f8d
Mickael Bolnet <mickael.bolnet@gmail.com>
2019-05-28 21:04:29 +0200

    first step

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+
 
 class App extends Component {
   render() {
@@ -13,6 +19,32 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Container>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>Name</th>
+                <th>Popularity</th>
+              </tr>
+            </thead>
+            <tbody>
+              {contacts.slice(0,5).map(person => (
+                <tr>
+                  <td>
+                    <Col xs={6} md={4}>
+                      <Image style={({width: '6em'})} src={person.pictureUrl} rounded />
+                    </Col>
+                  </td>
+
+                  <td>{person.name}</td>
+                  <td>{person.popularity}</td>
+                </tr>
+                )
+              )}
+            </tbody>
+          </Table>
+        </Container>
       </div>
     );
   }
