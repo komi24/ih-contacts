8b7f8ae97cda8357f88ce2c81d77dc6ec1a134f1
Mickael Bolnet <mickael.bolnet@gmail.com>
2019-05-30 19:41:30 +0200

    done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -4,12 +4,42 @@ import './App.css';
 import contacts from './contacts.json';
 import Table from 'react-bootstrap/Table';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 
 class App extends Component {
+  state = {
+    displayedContacts: contacts.slice(0,5)
+  }
+
+  addRandomContact() {
+    let random = Math.floor(Math.random()*contacts.length)
+    this.state.displayedContacts.push(contacts[random])
+    this.setState({displayedContacts: this.state.displayedContacts})
+  }
+
+  sortByName() {
+    this.setState({
+      displayedContacts: this.state.displayedContacts.sort((a,b)=>a.name<b.name?-1:1)
+    })
+  }
+
+  sortByPopularity() {
+    this.setState({
+      displayedContacts: this.state.displayedContacts.sort((a,b)=>a.popularity<b.popularity?-1:1)
+    })
+  }
+
+  deleteItem(index) {
+    this.state.displayedContacts.splice(index, 1)
+    this.setState({displayedContacts: this.state.displayedContacts})
+
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -20,17 +50,21 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Container>
+          <Button onClick={this.addRandomContact.bind(this)}>Add Random Contacts</Button>
+          <Button onClick={this.sortByName.bind(this)}>Sort By Name</Button>
+          <Button onClick={this.sortByPopularity.bind(this)}>Sort By Popularity</Button>
           <Table striped bordered hover>
             <thead>
               <tr>
                 <th>Picture</th>
                 <th>Name</th>
                 <th>Popularity</th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
-              {contacts.slice(0,5).map(person => (
-                <tr>
+              {this.state.displayedContacts.map((person,index) => (
+                <tr key={index}>
                   <td>
                     <Col xs={6} md={4}>
                       <Image style={({width: '6em'})} src={person.pictureUrl} rounded />
@@ -39,6 +73,12 @@ class App extends Component {
 
                   <td>{person.name}</td>
                   <td>{person.popularity}</td>
+                  <td>
+                    <Button 
+                      variant="danger"
+                      onClick={() => this.deleteItem(index)}
+                    >Delete</Button>
+                  </td>
                 </tr>
                 )
               )}
